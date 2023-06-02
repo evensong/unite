f4b95b177eaee282249977a5375cf5ed12c31a1d
evensong <benjamin.haws216@gmail.com>
2023-06-02 11:20:28 -0600

    Added dictionary, partially finished search and replace function

--- pynite/unite.py
+++ pynite/unite.py
@@ -1,16 +1,28 @@
 import PyPDF2
+import codecs # for spanish characters
+import re
 # from fpdf import FPDF
 
-with open('dictionary.txt') as translate:
-    path = input('Please enter document filepath: ')
+#read english/spanish word pairs
+dictionary = {}
+with codecs.open('dictionary.txt', encoding='utf-8') as translate:
+    
+    for line in translate:
+        english, spanish = line.split(':')
+        dictionary[english] = spanish.strip()
 
-    pdf_reader  = PyPDF2.PdfReader(open(path, 'rb'))
-
-    for page in pdf_reader.pages:
-        text = page.extract_text()
-        print(text, '\n') # Just for testing
-
-        #for english, spanish in translate.items():
-            #spanish_text = text.replace(english, spanish)
         
-        #print(spanish_text, '\n') # Just for testing
+path = input('Please enter document filepath: ')
+
+pdf_reader  = PyPDF2.PdfReader(open(path, 'rb'))
+
+text = ''
+spanish_text = ''    
+for page in pdf_reader.pages:
+    text += page.extract_text()
+
+for english, spanish in dictionary.items():
+        pattern = re.compile(english, flags=re.IGNORECASE)
+        spanish_text = pattern.sub(spanish, text)
+        
+print(spanish_text, '\n') # Just for testing
