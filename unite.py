65a60c4c30d298f5fe6ba27cc557e184b68a4fad
evensong <33677443+evensong@users.noreply.github.com>
2023-06-12 10:04:44 -0600

    Update unite.py
    Add GUI interface (push wasn't working)

--- unite.py
+++ unite.py
@@ -1,51 +1,32 @@
-import os
-import PyPDF2
-import codecs # for spanish characters
-import re
-import requests # get dictionary from github pages
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 10 21:38:11 2023
 
-dictionary = {}
+@author: evensong
+"""
 
-# get updated dictionary from the web, then read in word mapping
-response = requests.get('https://evensong.github.io/unite/dictionary.txt')
+from translator import Translator
+import tkinter as tk
+from tkinter import filedialog, simpledialog
 
-with open('english-spanish.txt', 'w+') as update:
-    update.write(response.text)
     
-with codecs.open('english-spanish.txt', encoding='utf-8') as translate:
+def main():
+    interpreter = Translator('spanish') # TODO: allow other languages 
     
-    for line in translate:
-        english, spanish = line.split(':')
-        dictionary[english] = spanish.strip()
-        
-# Read in the PDF        
-path = input('Please enter document filepath: ')
+    # Hide basse window
+    root = tk.Tk()
+    root.withdraw()
+    
+    # Get filepath and recipient's name
+    path = filedialog.askopenfilename(title="Bishop's Order File",
+                                      filetypes=[('pdf file', '*.pdf')])
+    name = simpledialog.askstring(title='Recipient Name', 
+                                  prompt="Please enter recipient's name")
+    
+    interpreter.get_file(path, name)
+    
+    interpreter.translate()
 
-pdf_reader  = PyPDF2.PdfReader(open(path, 'rb'))
-
-# Extract Text from PDF
-text = ''
-spanish_text = ''    
-for page in pdf_reader.pages:
-    text += page.extract_text()
-
-# Remove header and footer text to declutter output
-text = text.partition('OrderedQty\nFilled ')[2]
-text = text.partition('FOR INFORMATION ONLY')[0]
-text = text.replace('Ordered', '')
-text = text.replace('Qty', '')
-text = text.replace('Filled', '')
-
-# Replace English with Spanish based on the dictionary file
-for english, spanish in dictionary.items():
-        pattern = re.compile(english, flags=re.IGNORECASE)
-        spanish_text = pattern.sub(spanish, text)
-        text = spanish_text
-        
-#print(spanish_text, '\n') # Just for testing
-
-desktop = os.path.expanduser("~/Desktop")
-name = input('Please enter order recipient\'s name: ')
-with open('{}/{}-translation.txt'.format(desktop, name), 'w+') as output:
-    output.write(spanish_text)
-
+if __name__ == '__main__':
+    main()
